c43d75bb2e37f1e6de86a4605768783d025de70b
MarcusVinicius14 <marcus.vas14@gmail.com>
2023-10-02 11:18:27 -0300

    adicionando codeburger

--- src/Components/Projects/projects.js
+++ src/Components/Projects/projects.js
@@ -1,3 +1,4 @@
+import codeBurguer from '../../assets/codeBurguer.jpeg'
 import devMovie from '../../assets/devmovie.jpeg'
 import instagram from '../../assets/instagram.jpeg'
 import landingpage from '../../assets/landingpage.jpeg'
@@ -33,5 +34,12 @@ export const ProjectsData = [
     fornecem possibilidades tanto para usuários Desktop quanto Mobile.`,
     image: landingpage,
     link: 'https://landing-page-halloween-marcus.netlify.app'
+  },
+  {
+    id: 4,
+    name: 'E-Commerce CodeBurguer',
+    about: `Esse projeto é um E-Commerce de uma hambúrgueria que permite a criação de produtos, categorias, pedidos usuários... Ele possui uma API própria e dois banco de dados, um relacional (PostgreSQL) e outro não relacional (MongoDB), ambos com deploy na nuvem para que fique acessível para qualquer pessoa. Além disso possui uma interface feita em React e diversos react hooks.`,
+    image: codeBurguer,
+    link: 'https://code-burguerinterface.vercel.app/login'
   }
 ]
